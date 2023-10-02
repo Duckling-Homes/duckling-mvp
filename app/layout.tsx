bf3d85d3878170ad33be2d6f7f07135e96dc70db
Rahul Patni <rahul20patni@gmail.com>
2023-10-01 18:55:09 -0700

    adding clerk provider and user model

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Metadata } from "next";
 // import ducklingTheme from "./style/theme/theme" // Here whenever we decide to move to our own theme
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  manifest: '/manifest.json',
-}
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({
   children,
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
