1ca8f1884d0f4ddee1ba15c8267ba05b058e3ea2
Axel <axel@getduckling.com>
2023-10-04 21:27:44 -0700

    add back the samples

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -2,6 +2,12 @@ import withErrorHandler from "@/app/utils/withErrorHandler";
 import { createProject, getProjects } from "./repository";
 import { NextRequest, NextResponse } from "next/server";
 
+
+
+/**
+ * Create a project
+ * example: curl -X POST http://localhost:3000/api/projects -d '{"name":"Renovation Seattle", "homeownerName":"Rahul Patni", "homeownerPhone":"123-231-1233", "homeownerEmail":"asdf@asdf.com"}' -H "Content-Type: application/json"
+ */
 export const POST = withErrorHandler(async (req: NextRequest) => {
   const {
     name,
@@ -22,6 +28,10 @@ export const POST = withErrorHandler(async (req: NextRequest) => {
   );
 });
 
+/**
+ * Get all projects
+ * exmaple: curl http://localhost:3000/api/projects
+ */
 export const GET = withErrorHandler(async () => {
   return NextResponse.json(await getProjects());
 });
