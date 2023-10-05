3aa3a1d81e12575a694e8dc57522ab0598a18d4a
Charles Yu <charles.yu108@gmail.com>
2023-10-04 22:27:15 -0700

    fix up worker file

--- worker/index.ts
+++ worker/index.ts
@@ -1,34 +1,41 @@
-// import { util } from './util';
+console.log("CUSTOM WORKER REGISTERED")
+import { registerRoute, Route } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+// Handle images:
+const imageRoute = new Route(({ request }) => {
+  return request.destination === 'image'
+}, new StaleWhileRevalidate({
+  cacheName: 'images'
+}));
+
+// Handle scripts:
+const scriptsRoute = new Route(({ request }) => {
+  return request.destination === 'script';
+}, new CacheFirst({
+  cacheName: 'scripts'
+}));
+
+// Handle styles:
+const stylesRoute = new Route(({ request }) => {
+  return request.destination === 'style';
+}, new CacheFirst({
+  cacheName: 'styles'
+}));
 
 
-// util();
+// --------- GLOBALS ------------
+const globalContextRoute = new Route(({ request}) => {
+    console.log("HIT A MATCH FOR ALL PROJECTS");
+    return (request.method === 'GET' && request.url.endsWith('/api/projects/'))
+},
+    new StaleWhileRevalidate({
+        cacheName: 'globalContext'
+    })
+);
 
-console.log("RGISTERD")
-
-// self.addEventListener("fetch", event => {
-
-//     //@ts-ignore
-//     console.log(`URL requested: ${event.request.url}`);
-// });
-
-
-// // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
-// // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
-// //
-// // self.__WB_DISABLE_DEV_LOGS = true
-
-// console.log("REGISTERD SERVICE WORKER");
-
-// // listen to message event from window
-// self.addEventListener('message', event => {
-//   // HOW TO TEST THIS?
-//   // Run this in your browser console:
-//   //     window.navigator.serviceWorker.controller.postMessage({command: 'log', message: 'hello world'})
-//   // OR use next-pwa injected workbox object
-//   //     window.workbox.messageSW({command: 'log', message: 'hello world'})
-//   console.log(event?.data);
-// });
-
-// self.addEventListener('fetch', (event) => {
-//     console.log("GOT FETCH EVENT", event);
-// })
+// Register routes
+registerRoute(imageRoute);
+registerRoute(scriptsRoute);
+registerRoute(stylesRoute);
+registerRoute(globalContextRoute);
