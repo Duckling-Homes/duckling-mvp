72635bcd8b809f445275c30a2e6a603d4adf8d04
Gabriel Moraes <moraes.madm@gmail.com>
2023-10-05 16:09:31 -0300

    Adds mocked data to Basics tab and prepare for integration with backend

--- app/project/[id]/Tabs/Basics.tsx
+++ app/project/[id]/Tabs/Basics.tsx
@@ -5,8 +5,23 @@ import { Button, Divider, FormControl, InputLabel, MenuItem, Select, TextField }
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useState } from "react";
+
+const TODAY = new Date();
+
+const MOCK_PROJECT = {
+  square_footage: '2500',
+  room_count: '8',
+  bathroom_count: '2',
+  bedroom_count: '3',
+  stories: '2',
+  year_built: '1975',
+  basement_type: 'Finished',
+}
 
 const Basics = ({ }) => {
+  const [data, setData] = useState(MOCK_PROJECT)
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div style={{
@@ -25,29 +40,43 @@ const Basics = ({ }) => {
             label="Square Footage"
             variant="outlined"
             placeholder='Square Footage'
+            type="number"
+            value={data.square_footage}
+            onWheel={() => document.activeElement.blur()}
           />
           <TextField
             id="outlined-basic"
             label="Number of Rooms"
             variant="outlined"
             placeholder='Number of Rooms'
+            type="number"
+            value={data.room_count}
+            onWheel={() => document.activeElement.blur()}
           />
           <TextField
             id="outlined-basic"
             label="Number of Bathrooms"
             variant="outlined"
             placeholder='Number of Bathrooms'
+            type="number"
+            value={data.bathroom_count}
+            onWheel={() => document.activeElement.blur()}
           />
           <TextField
             id="outlined-basic"
             label="Stories"
             variant="outlined"
             placeholder='Stories'
+            type="number"
+            value={data.stories}
+            onWheel={() => document.activeElement.blur()}
           />
           <DatePicker
             label={"Year Built"}
             openTo="year"
             views={['year']}
+            // value={data.year_built}
+            // maxDate={TODAY}
           />
           <FormControl fullWidth>
             <InputLabel id="basement-type-label">Basement Type</InputLabel>
@@ -55,11 +84,12 @@ const Basics = ({ }) => {
               labelId="basement-type-label"
               id="basement-type-select"
               label="Basement Type"
+              value={data.basement_type}
             >
-              <MenuItem value={0}>Finished</MenuItem>
-              <MenuItem value={1}>Unfinished</MenuItem>
-              <MenuItem value={2}>Crawlspace</MenuItem>
-              <MenuItem value={2}>No Basement</MenuItem>
+              <MenuItem value={'Finished'}>Finished</MenuItem>
+              <MenuItem value={'Unfinished'}>Unfinished</MenuItem>
+              <MenuItem value={'Crawlspace'}>Crawlspace</MenuItem>
+              <MenuItem value={'No Basement'}>No Basement</MenuItem>
             </Select>
           </FormControl>
         </form>
