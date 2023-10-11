5ebf06561c43bc24a00019eaad071468efb825da
Axel <axel@getduckling.com>
2023-10-10 20:37:13 -0700

    make an error response if we need to

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
@@ -12,7 +11,14 @@ export default authMiddleware({
 
     if (req.nextUrl.pathname.startsWith("/api")) {
       // if this is an api route, add this header so our back end can tenantize its apis
-      req.headers.set("organization-context", (auth.sessionClaims?.metadata as any)?.organization_id)
+      if ((auth.sessionClaims?.metadata as any)?.organization_id) {
+        req.headers.set("organization-context", (auth.sessionClaims?.metadata as any)?.organization_id)
+      } else {
+          return NextResponse.json(
+            { success: false, message: "User does not have organization_id in Auth metadata" },
+            { status: 401 }
+          );
+      }      
     }
   },
 });
