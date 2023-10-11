dee3e7166bcaac3f7218bf888c6a68ee45ec5dbd
Axel <axel@getduckling.com>
2023-10-10 20:59:31 -0700

    nuked pretty much any functionality of the custom fetch tbh

--- app/helpers/customFetch.ts
+++ app/helpers/customFetch.ts
@@ -1,9 +1,9 @@
 async function customFetch(url: string, options: RequestInit = {}) {
     const defaultHeaders = {
       'Content-Type': 'application/json',
-      // TODO fix this value when we have organizations and auth. Right now, we'll just use
-      // our local hardcoded value
-      'organization-context': 'a4b5aa52-274d-4b1e-8f6b-3828f74c72d3', 
+      // TODO add this back when we have a need for an org switcher. We would then validate the
+      // org is valid in the middleware layer. But for now, we can just inject the header from the middleware
+      // 'organization-context': 'a4b5aa52-274d-4b1e-8f6b-3828f74c72d3', 
     };
   
     const mergedOptions = {
