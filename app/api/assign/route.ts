b51c0f4dd6996b36bbbaecdf2ee3d1e4c84bc57f
Rahul Patni <rahul20patni@gmail.com>
2023-10-11 20:47:07 -0700

    Adding comments

--- app/api/assign/route.ts
+++ app/api/assign/route.ts
@@ -3,8 +3,12 @@ import { clerkClient } from '@clerk/nextjs'
 import { getAuth } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
+/**
+ * This endpoint is called during sign in and sign up. It redirects to the home page.
+ *
+ * In the process below, it assigns a user to an organization. This route is not protected by authMiddleware org ID check.
+ */
 export const GET = withErrorHandler(async (req: NextRequest) => {
-  console.log('hey ho')
   const { userId } = getAuth(req)
 
   if (!userId) return NextResponse.redirect('/sign-in')
@@ -20,6 +24,9 @@ export const GET = withErrorHandler(async (req: NextRequest) => {
 
   // const emailDomain = user.emailAddresses[0].emailAddress.split('@')[1]
 
+  // we can add additional validation here including not overriding the org if it's already set
+  // we can further validate that the user is allowed to be in this org
+
   if (!userId) return NextResponse.redirect('/sign-in')
   await clerkClient.users.updateUser(userId, {
     publicMetadata: {
@@ -27,6 +34,5 @@ export const GET = withErrorHandler(async (req: NextRequest) => {
     },
   })
 
-  // return a redirect to "/"
   return NextResponse.redirect(new URL('/', req.url))
 })
