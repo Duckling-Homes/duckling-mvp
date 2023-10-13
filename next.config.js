4c8ea777e27db16bca6bb7fb207c187560c80c38
Charles Yu <charles.yu108@gmail.com>
2023-10-13 12:51:29 -0700

    Update to forked next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
+// NOTE: We are using a fork of next-pwa because it is no longer maintained
+// and has support issues with Next13, which seemingly are resolved by this fork
+// per discussion https://github.com/shadowwalker/next-pwa/issues/424
+const withPWA = require('@ducanh2912/next-pwa').default({
     dest: 'public',
 })
 
