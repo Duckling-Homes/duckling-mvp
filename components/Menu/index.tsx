da07955d4264141b04faa94332050e3886de0c91
Gabriel Moraes <moraes.madm@gmail.com>
2023-10-17 17:56:28 -0300

    Adds a menuItem to redirect users to projects list when user is using phone

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import { Menu } from '@mui/material';
+import { checkDeviceType } from '@/hooks/checkDeviceType';
 
 interface CustomMenuProps {
   open: boolean;
@@ -12,6 +13,8 @@ interface CustomMenuProps {
 export default function CustomMenu({
   open, handleClose, anchorEl, handleSignout
 }: CustomMenuProps) {
+  const device = checkDeviceType();
+
   return (
     <Menu
       id="basic-menu"
@@ -22,6 +25,11 @@ export default function CustomMenu({
         'aria-labelledby': 'basic-button',
       }}
     >
+      {device === 'phone' &&
+        <MenuItem onClick={handleSignout}>
+          Projects List
+        </MenuItem>
+      }
       <MenuItem onClick={handleSignout}>Logout</MenuItem>
     </Menu>
   );
