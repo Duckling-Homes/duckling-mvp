a3fa707b2db0be50c66aee0188f6ee2d517764b5
Gabriel Moraes <moraes.madm@gmail.com>
2023-10-18 19:22:28 -0300

    Finishes the creation of electricals by type

--- app/project/[id]/Tabs/Electrical.tsx
+++ app/project/[id]/Tabs/Electrical.tsx
@@ -2,33 +2,91 @@
 
 // import ChipManager from "@/components/ChipManager";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ElectricalPanelForm from "./ElectricalForms/ElectricalPanelForm";
 import SolarPanelForm from "./ElectricalForms/SolarPanelForm";
 import BatteryForm from "./ElectricalForms/BatteryForm";
 import EVChargerForm from "./ElectricalForms/EVChargerForm";
 import GeneratorForm from "./ElectricalForms/GeneratorForm";
+import ChipManager from "@/components/ChipManager";
+import { ProjectElectrical } from "@/types/types";
 
 const TYPES = [
-  "Electrical Panel",
-  "Solar",
-  "Battery",
-  "EV Charger",
-  "Generator",
+  {name: "Electrical Panel", value: "panel"},
+  {name: "Solar", value: "solar"},
+  {name: "Battery", value: "battery"},
+  {name: "EV Charger", value: "evCharger"},
+  {name: "Generator", value: "generator"}
 ]
 
 const Electrical = ({ currentProject }) => {
+  const [electricals, setElectricals] = useState([])
   const [currentElectrical, setCurrentElectrical] = useState({
-    type: ''
+    id: "",
+    type: '',
   });
 
-  const handleInputChange = async (inputName: string, value: string | number) => {
-    const updatedData = { ...currentElectrical, [inputName]: value };
-    setCurrentElectrical(updatedData);
-  };
+  useEffect(() => {
+    if (currentProject && currentProject?.electrical) {
+      setElectricals(currentProject.electrical)
+      setCurrentElectrical(currentProject.electrical[0])
+    }
+  }, [currentProject])
+
+  function createElectrical() {
+
+    const newElectrical = {
+      id: '',
+      name: "New Electrical",
+    };
+
+    const newElectricalsList = [...electricals, newElectrical];
+    setElectricals(newElectricalsList);
+    setCurrentElectrical(newElectrical);
+  }
+
+  const handleTypeChange = (name: string, value: string) => {
+    const updatedElectrical = {...currentElectrical, [name]: value}
+    handlePostElectrical(updatedElectrical, value)
+  }
+
+  async function handlePostElectrical(updatedElectrical: ProjectElectrical, type: string) {
+
+    try {
+      const data = await fetch(`/api/electrical/${type}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            ...updatedElectrical,
+            projectId: currentProject.id
+          })
+      });
+
+      if (data.ok) {
+        console.log('New appliance created successfully.');
+
+        const updatedElectricals = electricals.map((electrical) => {
+          if (electrical.id === updatedElectrical.id) {
+            return { ...electrical, ...updatedElectrical };
+          }
+          return electrical;
+        });
+
+        setElectricals(updatedElectricals);
+
+        const response = await data.json()
+        const createdElectrical = {...response, type: updatedElectrical.type}
+        setCurrentElectrical(createdElectrical)
+      } else {
+        throw new Error('Failed to create a new appliance.');
+      }
+    } catch (error) {
+      console.error('Error creating a new appliance:', error);
+      throw error;
+    }
+  }
 
   const renderForm = () => {
-    switch(currentElectrical.type) {
+    switch(currentElectrical?.type) {
       case 'Electrical Panel':
         return (<ElectricalPanelForm />);
       case 'Solar':
@@ -52,6 +110,14 @@ const Electrical = ({ currentProject }) => {
         gap: '32px',
       }}
     >
+      <ChipManager
+        onDelete={() => console.log('aheuhea')}
+        onCreate={createElectrical}
+        chipType="Electrical"
+        chips={electricals}
+        currentChip={currentElectrical?.id}
+        onChipClick={(i: number) => setCurrentElectrical(electricals[i])}
+      />
       <div style={{
         width: '100%',
       }}>
@@ -66,12 +132,12 @@ const Electrical = ({ currentProject }) => {
               labelId="type-label"
               id="type-select"
               label="Type"
-              value={currentElectrical.type}
-              onChange={({ target }) => handleInputChange('type', target.value)}
+              value={currentElectrical?.type}
+              onChange={({ target }) => handleTypeChange('type', target.value)}
             >
               {
                 TYPES.map((type, i) => (
-                  <MenuItem key={i} value={type}>{type}</MenuItem>
+                  <MenuItem key={i} value={type.value}>{type.name}</MenuItem>
                 ))
               }
             </Select>
