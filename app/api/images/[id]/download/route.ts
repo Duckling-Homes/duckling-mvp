d7b5c97b5dd51688572a2d429bfa4f161cab14c5
Axel <axel@getduckling.com>
2023-10-24 14:17:56 -0700

    upload download

--- app/api/images/[id]/download/route.ts
+++ app/api/images/[id]/download/route.ts
@@ -1,47 +1,38 @@
 import { NextRequest, NextResponse } from 'next/server'
 import AWS from 'aws-sdk'
-import { Readable } from 'stream'
+import { PassThrough } from 'stream'
 import { ImageType, constructS3ImageKey, getS3Client } from '@/app/utils/s3'
 
 // Initialize the S3 client
 const s3 = getS3Client()
 
-export async function POST(
+export async function GET(
   request: NextRequest,
   context: any
-): Promise<NextResponse> {
+): Promise<NextResponse | void> {
   try {
     const { id } = context.params
     const imageType: ImageType =
       (request.nextUrl.searchParams.get('type') as ImageType) || 'ORIGINAL'
-    const buffer = await request.arrayBuffer()
-    const readableStream = new Readable({
-      read() {
-        this.push(Buffer.from(buffer))
-        this.push(null)
-      },
-    })
 
     const orgContext = request.headers.get('organization-context') || ''
     const s3Key = constructS3ImageKey(orgContext, imageType, id)
 
-    const uploadParams = {
+    const downloadParams = {
       Bucket: process.env.UPLOAD_BUCKET || 'image-uploads',
       Key: s3Key,
-      Body: readableStream,
     }
 
-    const uploadResult = await s3.upload(uploadParams).promise()
+    const s3Object = await s3.getObject(downloadParams).promise()
+    const buffer = s3Object.Body as Buffer
 
-    return new NextResponse(
-      JSON.stringify({ location: uploadResult.Location }),
-      {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
+    return new NextResponse(buffer.buffer, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/jpeg',
+        'Content-Disposition': `attachment; filename="${id}.jpg"`,
+      },
+    })
   } catch (err) {
     return new NextResponse((err as Error).message, {
       status: 500,
--- app/utils/s3.ts
+++ app/utils/s3.ts
@@ -1,9 +1,21 @@
-type ImageType = 'ORIGINAL' | 'CROPPED'
+import AWS from 'aws-sdk'
 
-function constructS3ImageKey(
+export type ImageType = 'ORIGINAL' | 'CROPPED'
+
+export function constructS3ImageKey(
   orgId: string,
   type: ImageType,
   id: string
 ): string {
   return `${orgId}/${type.toLowerCase()}/${id}`
+}
+
+export function getS3Client(): AWS.S3 {
+  return new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY || 'minio',
+    secretAccessKey: process.env.S3_SECRET_KEY || 'minio123',
+    endpoint: process.env.S3_ENDPOINT || 'http://localhost:9000',
+    s3ForcePathStyle: true,
+    signatureVersion: 'v4',
+  })
 }
