00ad0aa2468c7892d009cb815e29d4becd9db81f
Axel <axel@getduckling.com>
2023-10-24 09:38:32 -0700

    a lil prettier

--- app/api/images/[id]/route.ts
+++ app/api/images/[id]/route.ts
@@ -1,28 +1,34 @@
-import { getImageById } from '@/app/utils/repositories/image';
-import withErrorHandler from '@/app/utils/withErrorHandler';
-import { NextRequest, NextResponse } from 'next/server';
+import { getImageById } from '@/app/utils/repositories/image'
+import withErrorHandler from '@/app/utils/withErrorHandler'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = withErrorHandler(
-  async (req: NextRequest, { params: routeParams }: { params: { id: string } }) => {
+  async (
+    req: NextRequest,
+    { params: routeParams }: { params: { id: string } }
+  ) => {
     try {
-      const orgContext = req.headers.get('organization-context') || '';
+      const orgContext = req.headers.get('organization-context') || ''
 
       // Fetch from Database
-      const image = await getImageById(routeParams.id, orgContext);
+      const image = await getImageById(routeParams.id, orgContext)
 
       if (!image) {
-        return NextResponse.json({ message: 'Image not found' }, { status: 404 });
+        return NextResponse.json(
+          { message: 'Image not found' },
+          { status: 404 }
+        )
       }
 
       // Only return the database metadata
-      return NextResponse.json({ image });
+      return NextResponse.json({ image })
     } catch (err) {
       return new NextResponse((err as Error).message, {
         status: 500,
         headers: {
           'Content-Type': 'application/json',
         },
-      });
+      })
     }
   }
-);
+)
--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,37 +1,32 @@
-import { createImage } from '@/app/utils/repositories/image';
-import withErrorHandler from '@/app/utils/withErrorHandler';
-import { NextRequest, NextResponse } from 'next/server';
+import { createImage } from '@/app/utils/repositories/image'
+import withErrorHandler from '@/app/utils/withErrorHandler'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const POST = withErrorHandler(async (req: NextRequest) => {
-    const {
-      id,
-      projectId,
-      photoName,
-      homeownerNotes,
-      internalNotes,
-      associatedEntityId,
-    } = await req.json();
+  const {
+    id,
+    projectId,
+    photoName,
+    homeownerNotes,
+    internalNotes,
+    associatedEntityId,
+  } = await req.json()
 
-    const orgContext = req.headers.get('organization-context') || '';
+  const orgContext = req.headers.get('organization-context') || ''
 
-    // Insert into Database
-    const dbImage = await createImage({
-      id,
-      projectId,
-      photoName,
-      homeownerNotes,
-      internalNotes,
-      associatedEntityId,
-    }, orgContext);
+  // Insert into Database
 
-    const imageId = dbImage.id;
-
-    return NextResponse.json(await createImage({
-      id,
-      projectId,
-      photoName,
-      homeownerNotes,
-      internalNotes,
-      associatedEntityId,
-    }, orgContext));
-});
+  return NextResponse.json(
+    await createImage(
+      {
+        id,
+        projectId,
+        photoName,
+        homeownerNotes,
+        internalNotes,
+        associatedEntityId,
+      },
+      orgContext
+    )
+  )
+})
