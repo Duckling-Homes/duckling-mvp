3a353ab21c6e83e158f01ad73813e050a23604e8
Axel <axel@getduckling.com>
2023-10-23 22:01:58 -0700

    remove try catch git push

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -3,7 +3,6 @@ import withErrorHandler from '@/app/utils/withErrorHandler';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const POST = withErrorHandler(async (req: NextRequest) => {
-  try {
     const {
       id,
       projectId,
@@ -27,13 +26,12 @@ export const POST = withErrorHandler(async (req: NextRequest) => {
 
     const imageId = dbImage.id;
 
-    return NextResponse.json({ status: 'success', imageId });
-  } catch (err) {
-    return new NextResponse((err as Error).message, {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+    return NextResponse.json(await createImage({
+      id,
+      projectId,
+      photoName,
+      homeownerNotes,
+      internalNotes,
+      associatedEntityId,
+    }, orgContext));
 });
