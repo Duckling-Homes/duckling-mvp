a171096d68114dbfcb88c910816f8740e97fe19f
Axel <axel@getduckling.com>
2023-10-23 21:25:43 -0700

    add s3 handling

--- app/api/images/[id]/route.ts
+++ app/api/images/[id]/route.ts
@@ -1,16 +1,44 @@
 import { getImageById } from '@/app/utils/repositories/image'
 import withErrorHandler from '@/app/utils/withErrorHandler'
 import { NextRequest, NextResponse } from 'next/server'
+import AWS from 'aws-sdk';
+
+// Initialize the S3 client
+const s3 = new AWS.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY || 'minio',
+  secretAccessKey: process.env.S3_SECRET_KEY || 'minio123',
+  endpoint: process.env.S3_ENDPOINT || 'http://localhost:9000',
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
 
 export const GET = withErrorHandler(
-  async (req: NextRequest, { params }: { params: { id: string } }) => {
-    const orgContext = req.headers.get('organization-context')
-    const image = await getImageById(params.id, orgContext as string)
+  async (req: NextRequest, { params: routeParams }: { params: { id: string } }) => {
+    try {
+      const orgContext = req.headers.get('organization-context')
+      const image = await getImageById(routeParams.id, orgContext as string)
 
-    if (!image) {
-      return NextResponse.json({ message: `Image not found` }, { status: 404 })
+      if (!image) {
+        return NextResponse.json({ message: `Image not found` }, { status: 404 })
+      }
+
+      const s3Key: string = `${orgContext}/originalSize/${routeParams.id}.jpg`;
+      const s3Params = {
+        Bucket: process.env.UPLOAD_BUCKET || 'image-uploads',
+        Key: s3Key,
+      };
+
+      const s3Object = await s3.getObject(s3Params).promise();
+      const imageBlob = s3Object.Body;
+
+      return NextResponse.json({ image, imageBlob });
+    } catch (err) {
+      return new NextResponse((err as Error).message, {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     }
-
-    return NextResponse.json(image)
   }
-)
+);
--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,28 +1,68 @@
 import { createImage } from '@/app/utils/repositories/image'
 import withErrorHandler from '@/app/utils/withErrorHandler'
 import { NextRequest, NextResponse } from 'next/server'
+import AWS from 'aws-sdk';
+import { Readable } from 'stream';
+
+// Initialize the S3 client
+const s3 = new AWS.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY || 'minio',
+  secretAccessKey: process.env.S3_SECRET_KEY || 'minio123',
+  endpoint: process.env.S3_ENDPOINT || 'http://localhost:9000',
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+});
 
 export const POST = withErrorHandler(async (req: NextRequest) => {
-  const {
-    id,
-    projectId,
-    photoName,
-    homeownerNotes,
-    internalNotes,
-    associatedEntityId,
-  } = await req.json()
-  
-  const orgContext = req.headers.get('organization-context')
-
-  return NextResponse.json(
-    await createImage({
+  try {
+    const {
       id,
       projectId,
       photoName,
       homeownerNotes,
       internalNotes,
       associatedEntityId,
-      organizationContext: orgContext as string,
-    })
-  )
-})
+    } = await req.json();
+  
+    const orgContext = req.headers.get('organization-context') || '';
+
+    // Insert into Database
+    const dbImage = await createImage({
+      id,
+      projectId,
+      photoName,
+      homeownerNotes,
+      internalNotes,
+      associatedEntityId,
+    }, orgContext);
+
+    const imageId = dbImage.id;
+    const s3Key = `${orgContext}/originalSize/${imageId}.jpg`;
+
+    // Upload to S3
+    const buffer = await req.arrayBuffer();
+    const readableStream = new Readable({
+      read() {
+        this.push(Buffer.from(buffer));
+        this.push(null);
+      },
+    });
+  
+    const uploadParams = {
+      Bucket: process.env.UPLOAD_BUCKET || 'image-uploads',
+      Key: s3Key,
+      Body: readableStream,
+    };
+  
+    await s3.upload(uploadParams).promise();
+
+    return NextResponse.json({ status: 'success', s3Key, imageId });
+  } catch (err) {
+    return new NextResponse((err as Error).message, {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+});
