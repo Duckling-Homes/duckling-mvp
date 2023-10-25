1bb3d9d73d4ef7fbd5db33b30d4bc84298155ff1
Rahul Patni <rahul20patni@gmail.com>
2023-10-24 20:02:40 -0700

    more optimized write publishChanges

--- app/sync/utils.ts
+++ app/sync/utils.ts
@@ -1,33 +1,51 @@
-import { db } from './db';
-import debounce from 'lodash/debounce';
+import debounce from 'lodash/debounce'
+import { _Request, db } from './db'
 
 export const isOnline = () => {
-    return navigator?.onLine;
-};
+  return navigator?.onLine
+}
 
-// Returns true/false indicating if new changes published
 export const publishChanges = debounce(async () => {
-    if (!isOnline()) return false;
+  if (!isOnline()) return false
+  const allChanges = await db.requests.orderBy('added').toArray()
+  // the key is       const key = curr.options?.method + curr.url + curr.id
+  // only pick the latest change for each key
 
-    let nextReq;
-    let didChange = false;
-    do {
-        try {
-            nextReq = await db.peekNextRequest();
-            if (nextReq) {
-                await fetch(nextReq!.url, nextReq!.options);
-                await db.dequeueRequest(nextReq.id!);
-                console.log("Dequeued", nextReq.options?.method, nextReq.url, nextReq.id);
-                didChange = true;
-            }
-        } catch (err) {
-            console.error("REQUEST FAILED TO PUSH...", { nextReq, err });
-        }
-    } while (nextReq);
-    return didChange;
-}, 200);
+  console.log('===== allChanges', allChanges.length)
+
+  const changesByURL: Map<string, _Request> = new Map()
+
+  allChanges.forEach((curr) => {
+    const key = curr.options?.method + curr.url
+    changesByURL.set(key, curr)
+  })
+
+  const changes = Array.from(changesByURL.values())
+  console.log('===== Publishing changes...', changes.length)
+
+  for (const change of changes) {
+    try {
+      console.log(
+        '==== Publishing',
+        change.options?.method,
+        change.url,
+        change.id,
+        JSON.parse(change.options?.body?.toString() || '{}')
+      )
+      await fetch(change.url, change.options)
+    } catch (err) {
+      console.error('==== REQUEST FAILED TO PUSH...', { change, err })
+    }
+  }
+
+  for (const change of allChanges) {
+    await db.dequeueRequest(change.id!)
+  }
+
+  return true
+}, 200)
 
 export const synchronizedFetch = async (url: string, options?: RequestInit) => {
-    await publishChanges();
-    return fetch(url, options);
-};
+  await publishChanges()
+  return fetch(url, options)
+}
