c68ea1a11c617198536f895b9a4e016eea62c640
Axel <axel@getduckling.com>
2023-10-24 20:21:00 -0700

    repo methods

--- app/utils/repositories/image.ts
+++ app/utils/repositories/image.ts
@@ -1,39 +1,63 @@
-import { Prisma } from '@prisma/client';
-import prisma from '../../../lib/prisma';
+import { Prisma } from '@prisma/client'
+import prisma from '../../../lib/prisma'
 
+type ExtendedImageCreateInput = Prisma.ImageUncheckedCreateInput & {
+  isHeroPhoto?: boolean
+}
 
 export async function createImage(
-  imageData: Prisma.ImageUncheckedCreateInput,
+  imageData: ExtendedImageCreateInput,
   organizationContext: string
 ) {
   // Fetch the project details based on the projectId
   const project = await prisma.project.findUnique({
     where: { id: imageData.projectId },
-  });
+  })
 
   // Check if the project's organizationId matches the organizationContext
   if (project && project.organizationId === organizationContext) {
-    return await prisma.image.create({
+    // Create the image
+    const createdImage = await prisma.image.create({
       data: {
         id: imageData.id,
-        photoName: imageData.photoName,
+        name: imageData.name,
         homeownerNotes: imageData.homeownerNotes,
         internalNotes: imageData.internalNotes,
-        associatedEntityId: imageData.associatedEntityId,
+        roomId: imageData.roomId,
+        envelopeId: imageData.envelopeId,
+        applianceId: imageData.applianceId,
+        electricalId: imageData.electricalId,
         project: {
           connect: {
             id: imageData.projectId,
           },
         },
       },
-    });
+    })
+
+    // If imageData.isHeroImage is true, update the project's heroImage
+    if (imageData.isHeroPhoto) {
+      await prisma.project.update({
+        where: { id: imageData.projectId },
+        data: {
+          heroImage: {
+            connect: { id: createdImage.id },
+          },
+        },
+      })
+    }
+
+    return createdImage
   } else {
-    throw new Error('Organization context does not match');
+    throw new Error('Organization context does not match')
   }
 }
 
 // Get all images for a particular project
-export async function getImagesByProjectId(projectId: string, organizationContext: string) {
+export async function getImagesByProjectId(
+  projectId: string,
+  organizationContext: string
+) {
   return await prisma.image.findMany({
     where: {
       projectId,
@@ -41,20 +65,22 @@ export async function getImagesByProjectId(projectId: string, organizationContex
         organizationId: organizationContext,
       },
     },
-  });
+  })
 }
 
 // Get a single image by its ID
 export async function getImageById(id: string, organizationContext: string) {
-  return await prisma.image.findUnique({
-    where: { id },
-    include: { project: true },
-  }).then(image => {
-    if (image?.project.organizationId === organizationContext) {
-      return image;
-    }
-    return null
-  });
+  return await prisma.image
+    .findUnique({
+      where: { id },
+      include: { project: true },
+    })
+    .then((image) => {
+      if (image?.project.organizationId === organizationContext) {
+        return image
+      }
+      return null
+    })
 }
 
 // Delete an image by its ID
@@ -62,7 +88,7 @@ export async function deleteImage(id: string, organizationContext: string) {
   const image = await prisma.image.findUnique({
     where: { id },
     include: { project: true },
-  });
+  })
 
   if (image?.project.organizationId !== organizationContext) {
     return null
@@ -70,7 +96,7 @@ export async function deleteImage(id: string, organizationContext: string) {
 
   return await prisma.image.delete({
     where: { id },
-  });
+  })
 }
 
 // Update an image by its ID
@@ -82,7 +108,7 @@ export async function updateImage(
   const image = await prisma.image.findUnique({
     where: { id },
     include: { project: true },
-  });
+  })
 
   if (image?.project.organizationId !== organizationContext) {
     return null
@@ -91,10 +117,5 @@ export async function updateImage(
   return await prisma.image.update({
     where: { id },
     data: imageUpdates,
-  });
+  })
 }
-
-
-
-
-
