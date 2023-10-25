c21ea4558f145f43a76cdaea51bd17c5f954bfcc
kiley <kiley@getduckling.com>
2023-10-24 18:37:59 -0700

    take photo UI

---
+++ components/Modals/PhotoModal/PhotoForms/PhotoForm.tsx
@@ -0,0 +1,221 @@
+'use client'
+
+import {
+  Checkbox,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  FormControlLabel,
+  Button,
+} from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined'
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
+import { PhotoDetails, Project } from '@/types/types'
+
+interface PhotoFormProps {
+  onClose: () => void
+  currentPhoto?: PhotoDetails
+  project: Project
+  onChange: (key: string, value: string | number | boolean | undefined) => void
+
+  // might want the project so that I can get the rooms and appliances
+  // also pass in the photo details
+}
+
+const PhotoForm: React.FC<PhotoFormProps> = ({
+  onClose,
+  currentPhoto,
+  project,
+  onChange,
+}) => {
+  const rooms = project.rooms ?? []
+  const envelopes = project.envelopes ?? []
+  const appliances = project.appliances ?? []
+  const electrical = project.electrical ?? []
+
+  async function patchPhotoDetails(updatedPhoto = currentPhoto) {
+    // TODO: Add patch API call
+    console.log('Making a call to patch the photo details')
+    console.log(updatedPhoto)
+  }
+
+  function deletePhoto() {
+    // TODO: Add in API call to delete image and its details
+    console.log('deleteing the photo')
+    onChange('photoUrl', undefined)
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '24px',
+        overflowY: 'auto',
+        maxHeight: '600px',
+        padding: '20px',
+      }}
+    >
+      {/* top button display */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Button variant="contained" startIcon={<EditOutlinedIcon />}>
+          Edit
+        </Button>
+        <Button variant="contained" startIcon={<ContentCopyOutlinedIcon />}>
+          Duplicate
+        </Button>
+        <Button variant="contained" onClick={deletePhoto}>
+          <DeleteOutlineOutlinedIcon />
+        </Button>
+      </div>
+      {/* photo */}
+      <img
+        src={currentPhoto?.photoUrl}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '400px',
+          width: 'auto',
+          height: 'auto',
+        }}
+        alt="Preview"
+      />
+      {/* photo name */}
+      <FormControl fullWidth>
+        <TextField
+          id="outlined-basic"
+          label="Photo Name"
+          variant="outlined"
+          placeholder="Photo Name"
+          type="text"
+          onChange={(e) => onChange('name', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.name}
+        />
+      </FormControl>
+      {/* notes for homeowner */}
+      <FormControl fullWidth>
+        <TextField
+          id="outlined-basic"
+          label="Notes for Homeowner"
+          variant="outlined"
+          placeholder="Enter notes for homeowner"
+          type="text"
+          onChange={(e) => onChange('homeownerNotes', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.homeownerNotes}
+        />
+      </FormControl>
+      {/* internal notes */}
+      <FormControl fullWidth>
+        <TextField
+          id="outlined-basic"
+          label="Internal Notes"
+          variant="outlined"
+          placeholder="Enter internal notes"
+          type="text"
+          onChange={(e) => onChange('internalNotes', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.internalNotes}
+        />
+      </FormControl>
+      {/* room */}
+      <FormControl fullWidth>
+        <InputLabel id="room-label">Room</InputLabel>
+        <Select
+          labelId="room-label"
+          id="room-select"
+          label="Room"
+          onChange={(e) => onChange('room', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.room}
+        >
+          {rooms.map((room, i) => (
+            <MenuItem key={i} value={room.id}>
+              {room.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {/* issue */}
+      <FormControl fullWidth>
+        <InputLabel id="envelope-label">Envelope</InputLabel>
+        <Select
+          labelId="envelope-label"
+          id="envelope-select"
+          label="Envelope"
+          onChange={(e) => onChange('envelope', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.envelope}
+        >
+          {envelopes.map((envelope, i) => (
+            <MenuItem key={i} value={envelope.id}>
+              {envelope.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {/* appliance */}
+      <FormControl fullWidth>
+        <InputLabel id="appliance-label">Appliance</InputLabel>
+        <Select
+          labelId="appliance-label"
+          id="appliance-select"
+          label="Appliance"
+          onChange={(e) => onChange('appliance', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.appliance}
+        >
+          {appliances.map((appliance, i) => (
+            <MenuItem key={i} value={appliance.id}>
+              {appliance.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {/* electrical */}
+      <FormControl fullWidth>
+        <InputLabel id="electrical-label">Electrical</InputLabel>
+        <Select
+          labelId="electrical-label"
+          id="electrical-select"
+          label="Electrical"
+          onChange={(e) => onChange('electrical', e.target.value)}
+          onBlur={() => patchPhotoDetails()}
+          value={currentPhoto?.electrical}
+        >
+          {electrical.map((electrical_val, i) => (
+            <MenuItem key={i} value={electrical_val.id}>
+              {electrical_val.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {/* hero photo */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={currentPhoto?.isHeroPhoto}
+            onChange={(e) => onChange('isHeroPhoto', e.target.value)}
+            onBlur={() => patchPhotoDetails()}
+          />
+        }
+        label="Hero Photo for Project"
+        style={{ marginTop: '10px', paddingLeft: '10px' }}
+      />
+      <Button variant="contained" startIcon={<CheckIcon />} onClick={onClose}>
+        Finished
+      </Button>
+    </div>
+  )
+}
+
+export default PhotoForm
---
+++ components/Modals/PhotoModal/index.tsx
@@ -0,0 +1,93 @@
+import { Close } from '@mui/icons-material'
+import { IconButton, Modal } from '@mui/material'
+import { PhotoDetails, Project } from '@/types/types'
+import { useState } from 'react'
+import PhotoCapture from './PhotoForms/PhotoCapture'
+import PhotoForm from './PhotoForms/PhotoForm'
+
+const PhotoCaptureModal: React.FC<{
+  open: boolean
+  onClose: () => void
+  onConfirm: (projectId: string) => void
+  project: Project
+  photo?: PhotoDetails
+}> = ({ open, onConfirm, onClose, project, photo }) => {
+  const [currentPhoto, setCurrentPhoto] = useState<PhotoDetails>(photo ?? {})
+  const handleOnClose = () => {
+    setCurrentPhoto({})
+    onClose()
+  }
+
+  function handlePhotoChange(
+    inputName: string,
+    value: string | number | boolean | undefined
+  ) {
+    if (currentPhoto) {
+      const updatedPhoto = { ...currentPhoto, [inputName]: value }
+      setCurrentPhoto(updatedPhoto)
+    }
+  }
+
+  return (
+    <Modal
+      open={open}
+      className="createModal"
+      onClose={handleOnClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        className="createModal__content"
+        style={{
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'white',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          borderRadius: '0',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <div className="createModal__header">
+          <IconButton
+            sx={{
+              borderRadius: '4px',
+              border: '1px solid #2196F3',
+              backgroundColor: '#2196F3',
+              color: '#fff',
+              padding: '4px 10px',
+              position: 'absolute',
+              top: '15px',
+              right: '15px',
+              zIndex: 2,
+            }}
+            onClick={handleOnClose}
+            aria-label="close"
+          >
+            <Close />
+          </IconButton>
+          {!currentPhoto.photoUrl && (
+            <PhotoCapture onChange={handlePhotoChange}></PhotoCapture>
+          )}
+          {currentPhoto.photoUrl && (
+            <PhotoForm
+              onClose={handleOnClose}
+              currentPhoto={currentPhoto}
+              project={project}
+              onChange={handlePhotoChange}
+            ></PhotoForm>
+          )}
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default PhotoCaptureModal
