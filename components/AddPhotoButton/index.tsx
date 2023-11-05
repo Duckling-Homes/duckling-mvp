d409d674cd22bab0035ee09738cd8aae91447713
kiley-duckling <145923736+kiley-duckling@users.noreply.github.com>
2023-11-04 20:52:11 -0600

    Allow users to upload photos from device (#73)
    * upload photos feature
    
    * remove comment

--- components/AddPhotoButton/index.tsx
+++ components/AddPhotoButton/index.tsx
@@ -13,75 +13,118 @@ const AddPhotoButton: React.FC<{
     const [anchorOptions, setAnchorOptions] = useState<null | HTMLElement>(null)
     const [openCamera, setOpenCamera] = useState<boolean>(false)
     const [openPhotoPicker, setOpenPhotoPicker] = useState<boolean>(false)
+    const [isUploading, setIsUploading] = useState<boolean>(false)
 
     const buttonRef = useRef<HTMLButtonElement>(null)
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
     const currentProject = ModelStore.currentProject
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
       setAnchorOptions(event.currentTarget)
     }
-    
+
     const handleClose = () => {
       setAnchorOptions(null)
     }
 
+    const handleFileChange = async (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      const file = event.target.files ? event.target.files[0] : null
+      if (file && currentProject) {
+        setIsUploading(true)
+        const reader = new FileReader()
+        reader.onloadend = async () => {
+          const photoUrl = reader.result as string
+          await ModelStore.createPhotoEntry(currentProject.id!, photoUrl, {
+            ...photoUpdates,
+          })
+          setIsUploading(false)
+        }
+
+        reader.onerror = () => {
+          console.error('There was an error reading the file!')
+          setIsUploading(false)
+          reader.abort()
+        }
+
+        reader.readAsDataURL(file)
+      }
+      handleClose()
+    }
+
+    const handleUploadClick = () => {
+      fileInputRef.current?.click()
+      handleClose()
+    }
+
     const buttonWidth = buttonRef.current?.clientWidth
 
-
-  return (
-    <>
-      {currentProject && <PhotoCaptureModal
-        open={openCamera}
-        project={currentProject}
-        onClose={() => {
-            setOpenCamera(false)
-            handleClose()
-        }}
-        photo={{}}
-      />}
-      {photoUpdates && <PhotoPickerModal
-        open={openPhotoPicker}
-        onClose={() => {
-            setOpenPhotoPicker(false)
-            handleClose()
-        }}
-        photoUpdates={photoUpdates}
-      />}
-      <Button
-        variant="contained"
-        startIcon={<CameraAltOutlinedIcon />}
-        onClick={handleClick}
-        ref={buttonRef}
-        style={{
+    return (
+      <>
+        {currentProject && (
+          <PhotoCaptureModal
+            open={openCamera}
+            project={currentProject}
+            onClose={() => {
+              setOpenCamera(false)
+              handleClose()
+            }}
+            photo={{}}
+          />
+        )}
+        {photoUpdates && (
+          <PhotoPickerModal
+            open={openPhotoPicker}
+            onClose={() => {
+              setOpenPhotoPicker(false)
+              handleClose()
+            }}
+            photoUpdates={photoUpdates}
+          />
+        )}
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          style={{ display: 'none' }}
+          accept="image/*"
+        />
+        <Button
+          variant="contained"
+          startIcon={<CameraAltOutlinedIcon />}
+          onClick={handleClick}
+          ref={buttonRef}
+          disabled={isUploading}
+          style={{
             minWidth: '200px',
           }}
-      >
-        Add Photo
-      </Button>
-      <Menu
-        anchorEl={anchorOptions}
-        open={Boolean(anchorOptions)}
-        onClose={handleClose}
-        sx={{
+        >
+          Add Photo
+        </Button>
+        <Menu
+          anchorEl={anchorOptions}
+          open={Boolean(anchorOptions)}
+          onClose={handleClose}
+          sx={{
             '.MuiPaper-root': {
               minWidth: buttonWidth,
             },
           }}
-      >
-        <MenuItem 
-          onClick={() => setOpenCamera(true)}
         >
-          Take new photo
-        </MenuItem>
-        <MenuItem onClick={handleClose}>Upload from device</MenuItem>
-        {photoUpdates && <MenuItem
-          onClick={() => setOpenPhotoPicker(true)}
-        >
-          Choose from project
-        </MenuItem>}
-      </Menu>
-    </>
-  )
+          <MenuItem onClick={() => setOpenCamera(true)}>
+            Take new photo
+          </MenuItem>
+          <MenuItem onClick={handleUploadClick}>Upload from device</MenuItem>
+          {photoUpdates && (
+            <MenuItem onClick={() => setOpenPhotoPicker(true)}>
+              Choose from project
+            </MenuItem>
+          )}
+        </Menu>
+      </>
+    )
 })
 
 export default AddPhotoButton
