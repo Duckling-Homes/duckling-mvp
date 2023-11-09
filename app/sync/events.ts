68e6ac423913092b66b5d7ec7e858360a8d1b7d4
Charles Yu <charles.yu108@gmail.com>
2023-11-09 14:41:49 -0800

    fix small photo bug

--- app/sync/events.ts
+++ app/sync/events.ts
@@ -1,9 +1,11 @@
+import { Project } from "@/types/types";
 import EventEmitter from "events";
 import TypedEmitter from "typed-emitter";
 
 type SyncAPIEventTypes = {
     'did-go-online': (at: number) => void,
     'did-go-offline': (at: number) => void,
+    // 'did-update-project': (projectID: string, value: Project | null) => void,
     'has-pending-changes': (status: boolean) => void,
 }
 
