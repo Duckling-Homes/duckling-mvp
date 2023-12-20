c9c9a2f840ddd1fa8c3e2752e72142e330102533
Rahul Patni <rahul20patni@gmail.com>
2023-12-19 18:15:04 -0800

    adding dummy so that the project builds

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,6 +1,8 @@
 import OpenAI from 'openai'
 
 // defaults to process.env["OPENAI_API_KEY"]
-const openai = new OpenAI({})
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || 'empty',
+})
 
 export default openai
