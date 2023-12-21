cc103b790866f478ead8427844458c9acdb5c070
kiley <kiley@getduckling.com>
2023-12-21 08:39:00 -0800

    thing

--- app/sync/operations/presentation.ts
+++ app/sync/operations/presentation.ts
@@ -3,7 +3,6 @@ import { synchronizedFetch } from '../utils'
 
 export class PresentationSyncOperations {
   get = syncAPImutation(async (projectID: string) => {
-    // This probably also needs to work on offline... so should build this object from all the other details
     const response = await synchronizedFetch(`/api/presentation/${projectID}`, {
       method: 'GET',
     })
