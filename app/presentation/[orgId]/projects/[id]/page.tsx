c990e9addd48d6bb7a06090ad0daf0bd47e4ac37
kiley-duckling <145923736+kiley-duckling@users.noreply.github.com>
2023-12-26 11:40:26 -0800

    create shareable page (#126)

---
+++ app/presentation/[orgId]/projects/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Container } from '@/components/Container'
+
+const ShareablePresentation = () => {
+  return (
+    <Container publicRoute={true}>
+      <div>presentation</div>
+    </Container>
+  )
+}
+
+export default ShareablePresentation
