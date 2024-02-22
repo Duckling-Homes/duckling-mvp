dbc2baa60a1729e5b910a9f7893a5e4a58b0ffbe
Rahul Patni <rahul20patni@gmail.com>
2024-02-22 08:20:00 -0800

    converting next js to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@
 // NOTE: We are using a fork of next-pwa because it is no longer maintained
 // and has support issues with Next13, which seemingly are resolved by this fork
 // per discussion https://github.com/shadowwalker/next-pwa/issues/424
-const withPWA = require('@ducanh2912/next-pwa').default({
+import * as withPWAInit from '@ducanh2912/next-pwa'
+
+const withPWA = withPWAInit.default({
   dest: 'public',
   fallbacks: {
     document: '/~offline',
@@ -18,4 +20,5 @@ const nextConfig = {
     return config
   },
 }
-module.exports = withPWA(nextConfig)
+
+export default withPWA(nextConfig)
