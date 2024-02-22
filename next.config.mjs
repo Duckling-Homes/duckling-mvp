925166bd688a57c28db3b29c23a6e4fb57c73dbb
Rahul Patni <rahul20patni@gmail.com>
2024-02-22 08:07:32 -0800

    wip

---
+++ next.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+
+import million from "million/compiler";
+
+// NOTE: We are using a fork of next-pwa because it is no longer maintained
+// and has support issues with Next13, which seemingly are resolved by this fork
+// per discussion https://github.com/shadowwalker/next-pwa/issues/424
+
+import * as withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit.default({
+  dest: 'public',
+  fallbacks: {
+    document: '/~offline',
+  },
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: false,
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }] // required to make Konva & react-konva work
+    return config
+  },
+}
+
+const millionConfig = {
+  auto: true,// if you're using RSC: auto: { rsc: true },
+};
+
+module.exports = withPWA(nextConfig)
+export default million.next(nextConfig, millionConfig);
+
+
