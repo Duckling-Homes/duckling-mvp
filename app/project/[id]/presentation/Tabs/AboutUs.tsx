8e558fa655816cad208a0fbd3e99de66b351b271
Gabriel Moraes <moraes.madm@gmail.com>
2024-03-13 09:38:19 -0300

    Fixes about us layout

--- app/project/[id]/presentation/Tabs/AboutUs.tsx
+++ app/project/[id]/presentation/Tabs/AboutUs.tsx
@@ -4,24 +4,32 @@ import { observer } from 'mobx-react-lite'
 const AboutUsPage: React.FC<{
   organization: Organization
 }> = observer(({ organization }) => {
+  const descr =
+    'Lorem ipsum dolor sit amet consectetur. Leo odio proin eget amet in elementum neque purus. Et bibendum et mollis felis diam sit laoreet tristique in. Vulputate vitae aliquet imperdiet malesuada erat.'
+
+  const webpage = 'www.google.com'
+
   return (
-    <>
-      <div className="aboutUs">
-        <div className="aboutUs">
-          <div className="aboutUs__header">
-            <p>{organization.name}</p>
-          </div>
-          <div className="aboutUs__text">
-            <p>{organization.description}</p>
-            {organization.webpage && (
-              <a href={organization.webpage} target="_blank">
-                Our website
-              </a>
-            )}
-          </div>
-        </div>
+    <div className="aboutUs">
+      <div className="aboutUs__image">
+        <img src="/assets/company-profile.png" />
       </div>
-    </>
+      <div className="aboutUs__content">
+        <div className="aboutUs__header">
+          <p>{organization.name}</p>
+        </div>
+        <div className="aboutUs__text">
+          <p>{descr}</p>
+        </div>
+        {webpage && (
+          <div>
+            <a href={webpage} target="_blank">
+              Our website
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
   )
 })
 
